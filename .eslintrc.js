b62f3820d7ffd7421549b29b26cb27604fedc8ae
hzzhaoxiangtao@corp.netease.com <hzzhaoxiangtao@corp.netease.com>
2019-10-30 21:42:43 +0800

    feat: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
+    env: {
+        es6: true,
+    },
+    extends: 'eslint:recommended',
     rules: {
         indent: ['warn', 4, { SwitchCase: 0 }],
         'no-script-url': 'off',
     },
+    plugins: ['react'],
 };
