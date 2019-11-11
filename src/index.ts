9069d981062a137b73de1e9b61de17c8f54adad3
zwd <zwd321081@gmail.com>
2019-11-11 11:48:46 +0800

    feat: do some change and replace ssr to isomorphic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import catchreacterror from './catch-react-error'
 
 export * from './components/csr/Errorboundary'
-export * from './components/ssr/Errorboundary'
+export * from './components/iso/Errorboundary'
 export * from './interface/propsInterface'
 
 export default catchreacterror
