4a760f10ca88a464038736774e75b49c8852087e
hzzhaoxiangtao@corp.netease.com <hzzhaoxiangtao@corp.netease.com>
2019-12-17 22:21:06 +0800

    feat: 添加新的测试代码

--- src/__tests__/ErrorBoundary.test.js
+++ src/__tests__/ErrorBoundary.test.js
@@ -1,64 +1,65 @@
-import React from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
-import Enzyme, { shallow, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+import Enzyme, { shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import { IsomorphicErrorBoundary, is_server, serverMarkup } from '../../dist';
+import { IsomorphicErrorBoundary, serverMarkup } from "../../dist";
+import { is_server } from "../../dist/util";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('is_server in server', () => {
-    it('should is_server be false', () => {
-        expect(is_server()).toBeFalsy();
-    });
+describe("is_server in server", () => {
+  it("should is_server be false", () => {
+    expect(is_server()).toBeFalsy();
+  });
 });
 
-describe('When no JS errors are caught in a child component', () => {
-    let wrapper;
+describe("When no JS errors are caught in a child component", () => {
+  let wrapper;
 
-    beforeAll(() => {
-        wrapper = shallow(
-            <IsomorphicErrorBoundary>
-                <h1>wassup</h1>
-            </IsomorphicErrorBoundary>
-        );
-    });
+  beforeAll(() => {
+    wrapper = shallow(
+      <IsomorphicErrorBoundary>
+        <h1>wassup</h1>
+      </IsomorphicErrorBoundary>
+    );
+  });
 
-    it('should update the state to indicate an error', () => {
-        expect(wrapper.state().hasError).toBeFalsy();
-    });
+  it("should update the state to indicate an error", () => {
+    expect(wrapper.state().hasError).toBeFalsy();
+  });
 
-    it('should render the child component', () => {
-        expect(wrapper.find('h1').exists()).toBeTruthy();
-    });
+  it("should render the child component", () => {
+    expect(wrapper.find("h1").exists()).toBeTruthy();
+  });
 });
 
-describe('When a JS error is caught in a child component', () => {
-    let wrapper;
+describe("When a JS error is caught in a child component", () => {
+  let wrapper;
 
-    beforeAll(() => {
-        jest.spyOn(global.console, 'log');
+  beforeAll(() => {
+    jest.spyOn(global.console, "log");
 
-        const ProblemChild = () => {
-            throw new Error('Error thrown from problem child');
-            return <div>Error</div>;
-        };
-        wrapper = mount(
-            <IsomorphicErrorBoundary>
-                <ProblemChild />
-            </IsomorphicErrorBoundary>
-        );
-    });
+    const ProblemChild = () => {
+      throw new Error("Error thrown from problem child");
+      return <div>Error</div>;
+    };
+    wrapper = mount(
+      <IsomorphicErrorBoundary>
+        <ProblemChild />
+      </IsomorphicErrorBoundary>
+    );
+  });
 
-    it('should log an error to console', () => {
-        expect(global.console.log).toHaveBeenCalledTimes(1);
-    });
+  it("should log an error to console", () => {
+    expect(global.console.log).toHaveBeenCalledTimes(1);
+  });
 
-    it('should update the state to indicate an error', () => {
-        expect(wrapper.state().hasError).toBeTruthy();
-    });
+  it("should update the state to indicate an error", () => {
+    expect(wrapper.state().hasError).toBeTruthy();
+  });
 
-    it('should not render the child component', () => {
-        expect(wrapper.find('h1').exists()).toBeFalsy();
-    });
+  it("should not render the child component", () => {
+    expect(wrapper.find("h1").exists()).toBeFalsy();
+  });
 });
