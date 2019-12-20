e661de5dcbbb4567c0bd16e3cb97b49c806dd4f0
Sylvenas <hzzhaoxiangtao@corp.netease.com>
2019-12-20 20:10:22 +0800

    feat: collectCoverage

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    testMatch: [
-        '**/__tests__/**/*.+(ts|tsx|js)',
-        '**/?(*.)+(spec|test).+(ts|tsx|js)',
-    ],
-    transform: {
-        '^.+\\.(ts|tsx)?$': 'ts-jest',
-        '^.+\\.[t|j]sx?$': 'babel-jest',
-    },
+  roots: ["<rootDir>/src"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.[t|j]sx?$": "babel-jest"
+  },
+  collectCoverage: true
 };
