2277437a7b10468097ca65035007394bf97ccbcd
Sylvenas <hzzhaoxiangtao@corp.netease.com>
2020-07-08 18:47:45 +0800

    feat: RN adaptation

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,11 @@
 export function is_server(): boolean {
-    return !(typeof window !== 'undefined' && window.document);
+    if (typeof window !== 'undefined' && window.document) {
+        return false // web browser
+    } else if (typeof navigator != 'undefined' && navigator.product == 'ReactNative') {
+        return false // ReactNative https://github.com/facebook/react-native/issues/1331#issuecomment-183903948
+    } else {
+        return true // node.js
+    }
 }
 
 export function isComponentClass<T>(Component: React.ComponentClass<T> | React.StatelessComponent<T>): Component is React.ComponentClass<T> {
